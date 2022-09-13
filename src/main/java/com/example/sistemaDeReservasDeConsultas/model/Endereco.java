56dec491a9df2a2a6e93b3254e377bd202ea2259
Thribs <thribsilva@gmail.com>
2022-09-12 21:52:27 -0300

    Revert "refatorando para diminuir acoplamento"
    This reverts commit 1073d1fd514cc67ec9504f91a0e0e37974841c78.

--- src/main/java/com/example/sistemaDeReservasDeConsultas/model/Endereco.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/model/Endereco.java
@@ -1,6 +1,9 @@
 package com.example.sistemaDeReservasDeConsultas.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -12,13 +15,25 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @Entity
-public class Endereco extends BaseEntity {
+public class Endereco {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private @Getter @Setter Long id;
     private @Getter @Setter String rua;
     private @Getter @Setter String numero;
     private @Getter @Setter String bairro;
     private @Getter @Setter String cidade;
     private @Getter @Setter String uf;
     private @Getter @Setter String pais;
+
+    public Endereco(String rua, String numero, String bairro, String cidade, String uf, String pais) {
+        this.rua = rua;
+        this.numero = numero;
+        this.bairro = bairro;
+        this.cidade = cidade;
+        this.uf = uf;
+        this.pais = pais;
+    }
 
 }
--- src/main/java/com/example/sistemaDeReservasDeConsultas/model/Paciente.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/model/Paciente.java
@@ -3,6 +3,9 @@ package com.example.sistemaDeReservasDeConsultas.model;
 import java.sql.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
 import javax.persistence.FetchType;
@@ -18,12 +21,25 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 @Entity
-public class Paciente extends Pessoa {
+public class Paciente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private @Getter @Setter Long id;
+    private @Getter @Setter String nome;
+    private @Getter @Setter String sobrenome;
     @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id")
     private @Getter @Setter Endereco endereco;
     private @Getter @Setter String rg;
     private @Getter @Setter Date dataDeAlta;
+
+    public Paciente(String nome, String sobrenome, Endereco endereco, String rg, Date dataDeAlta) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.rg = rg;
+        this.dataDeAlta = dataDeAlta;
+    }
+
     
 }
