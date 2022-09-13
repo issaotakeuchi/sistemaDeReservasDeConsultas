c90b6c30c58567db3daf29991cb9b61b31da5957
ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>
2022-09-13 15:46:28 -0300

    Criação de consultas (ajustes em classes para relacionamento one to many e many to one
    Signed-off-by: ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>

--- src/main/java/com/example/sistemaDeReservasDeConsultas/model/Consulta.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/model/Consulta.java
@@ -1,32 +1,29 @@
-package com.example.sistemaDeReservasDeConsultas.entity;
+package com.example.sistemaDeReservasDeConsultas.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Date;
 
 
-@AllArgsConstructor
 @Data
-@Entity
+@AllArgsConstructor
 @NoArgsConstructor
+@Entity
 @Table(name = "Consulta")
-public class ConsultaModel {
+public class Consulta {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "paciente_id")
-    private PacienteEntity paciente;
+    private Paciente paciente;
     @ManyToOne
     @JoinColumn(name = "dentista_id")
-    private DentistaEntity dentista;
+    private Dentista dentista;
     @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dataEHoraConsulta;
 }
--- src/main/java/com/example/sistemaDeReservasDeConsultas/service/ConsultaServiceImpl.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/service/ConsultaServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.sistemaDeReservasDeConsultas.service;
 
-import com.example.sistemaDeReservasDeConsultas.entity.ConsultaModel;
+import com.example.sistemaDeReservasDeConsultas.model.Consulta;
 import com.example.sistemaDeReservasDeConsultas.repository.IConsultaRepository;
 import org.springframework.stereotype.Service;
 
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Service
-public class ConsultaServiceImpl implements IConsultaService<ConsultaModel> {
+public class ConsultaServiceImpl implements IService<Consulta> {
     private final IConsultaRepository repository;
 
     public ConsultaServiceImpl(IConsultaRepository repository) {
@@ -16,40 +16,37 @@ public class ConsultaServiceImpl implements IConsultaService<ConsultaModel> {
     }
 
     @Override
-    public List<ConsultaModel> getAll() {
+    public Consulta add(Consulta consulta) {
+        if (consulta != null) { return repository.save(consulta); }
+        return new Consulta();
+    }
+
+    @Override
+    public List<Consulta> getAll() {
         return repository.findAll();
     }
 
     @Override
-    public Optional<ConsultaModel> getById(Long id) {
+    public Optional<Consulta> getById(Long id) {
         return repository.findById(id);
     }
 
     @Override
-    public ConsultaModel add(ConsultaModel consulta) {
-        if (consulta != null) {
-            return repository.save(consulta);
-        }
-        return new ConsultaModel();
+    public void remove(Long id) {
+        if (getById(id).isPresent()) { repository.deleteById(id); }
+        else {
+            throw new IllegalStateException("Repositório nulo"); }
     }
 
     @Override
-    public void update(ConsultaModel consulta) {
-        if (consulta == null) {
-            throw new IllegalStateException("Consulta nulo");
-        } else if (consulta != null && repository.findById(consulta.getId()).isPresent()) {
+    public void update(Consulta consulta) {
+        if (consulta == null) { throw new IllegalStateException("Consulta nulo"); }
+        else if (consulta != null && repository.findById(consulta.getId()).isPresent()) {
             repository.saveAndFlush(consulta);
         }
     }
 
-    @Override
-    public void remove(Long id) {
-        if (getById(id).isPresent()) {
-            repository.deleteById(id);
-        } else {
-            throw new IllegalStateException("Repositório nulo");
-        }
-    }
+
 }
 
 
