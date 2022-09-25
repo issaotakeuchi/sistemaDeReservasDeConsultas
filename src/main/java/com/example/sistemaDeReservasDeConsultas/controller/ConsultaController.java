3915fefbcc2372e08ad730d5a1a4ff476ba1e22a
ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>
2022-09-25 15:13:13 -0300

    Criação de logs
    Signed-off-by: ISSAO TORIBIO TAKEUCHI <issaottakeuchi@gmail.com>

--- src/main/java/com/example/sistemaDeReservasDeConsultas/controller/ConsultaController.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/controller/ConsultaController.java
@@ -4,6 +4,8 @@ import com.example.sistemaDeReservasDeConsultas.exceptions.BadRequestException;
 import com.example.sistemaDeReservasDeConsultas.exceptions.ResourceNotFoundException;
 import com.example.sistemaDeReservasDeConsultas.model.Consulta;
 import com.example.sistemaDeReservasDeConsultas.service.ConsultaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,7 @@ import java.util.List;
 @RequestMapping("/consultas")
 public class ConsultaController {
     private final ConsultaService service;
+    final static Logger log = LoggerFactory.getLogger(ConsultaController.class);
 
     public ConsultaController(ConsultaService service) {
         this.service = service;
@@ -22,13 +25,15 @@ public class ConsultaController {
     @PostMapping
     public ResponseEntity<Consulta> cadastrarConsulta(@RequestBody Consulta consulta) throws BadRequestException {
         try {
+            log.info("Cadastrada nova consulta com sucesso.");
             return ResponseEntity.ok(service.add(consulta));
         } catch (Exception e) {
+            log.info("Não foi possível cadastrar a consulta com base nas informações recebidas.");
             throw new BadRequestException("Os dados da solicitação não correspondem aos necessários para o cadastro.");
         }
     }
 
-    @RequestMapping
+    @GetMapping
     public List<Consulta> buscarTodos() {
         return service.getAll();
     }
@@ -36,8 +41,11 @@ public class ConsultaController {
     @GetMapping("/{id}")
     public ResponseEntity<Consulta> buscarConsultaId(@PathVariable Long id) throws ResourceNotFoundException {
         try {
+            service.getById(id);
+            log.info("Encontrada a consulta solicitada de id: " + id);
             return ResponseEntity.ok(service.getById(id));
         } catch (Exception e) {
+            log.info("Não foi encontrado a consulta solicitada de id: " + id);
             throw new ResourceNotFoundException("Não foi encontrado a consulta buscada com id: " + id);
         }
     }
@@ -45,8 +53,12 @@ public class ConsultaController {
     @PutMapping("atualizar")
     public ResponseEntity<Consulta> alterarConsulta(@RequestBody Consulta consulta) throws BadRequestException {
         try {
+            service.getById(consulta.getId());
+            service.update(consulta);
+            log.info("A consulta de id: " + consulta.getId() + " foi atualizada com sucesso.");
             return ResponseEntity.ok(service.update(consulta));
         } catch (Exception e) {
+            log.info("Não foi possível atualizar a consulta.");
             throw new BadRequestException("Não foi possível atualizar os dados da consulta com os dados desta requisição");
         }
     }
@@ -56,8 +68,10 @@ public class ConsultaController {
         try {
             ResponseEntity.ok(service.getById(id));
             service.remove(id);
+            log.info("A consulta de id: " + id + " foi localizada no banco de dados e removida com sucesso.");
             return ResponseEntity.ok("Consulta excluída.");
         } catch (Exception e) {
+            log.info("Não foi encontrado a consulta de id " + id + " para efetuar a exclusão.");
             throw new ResourceNotFoundException("Não foi possível excluir a consulta de id: " + id);
         }
     }
--- src/main/java/com/example/sistemaDeReservasDeConsultas/controller/PacienteController.java
+++ src/main/java/com/example/sistemaDeReservasDeConsultas/controller/PacienteController.java
@@ -4,6 +4,8 @@ import com.example.sistemaDeReservasDeConsultas.exceptions.ResourceNotFoundExcep
 import com.example.sistemaDeReservasDeConsultas.model.Paciente;
 import com.example.sistemaDeReservasDeConsultas.service.PacienteService;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,14 +17,17 @@ import java.util.List;
 public class PacienteController {
 
     private final PacienteService service;
+    final static Logger log = LoggerFactory.getLogger(PacienteController.class);
 
     public PacienteController(PacienteService service) { this.service = service; }
 
     @PostMapping
     public ResponseEntity<Paciente> cadastrarPaciente(@RequestBody Paciente paciente) throws BadRequestException {
         try {
+            log.info("Cadastrado com sucesso.");
             return ResponseEntity.ok(service.add(paciente));
         } catch (Exception e) {
+            log.info("Dados para cadastro não correspondem ao necessário.");
             throw new BadRequestException("Os dados da solicitação não correspondem aos necessários para o cadastro.");
         }
     }
@@ -35,8 +40,11 @@ public class PacienteController {
     @GetMapping("/{id}")
     public ResponseEntity<Paciente> buscaPacienteId(@PathVariable Long id) throws ResourceNotFoundException {
         try {
+            service.getById(id);
+            log.info("Encontrado o paciente de id: " + id);
             return ResponseEntity.ok(service.getById(id));
         } catch (Exception e) {
+            log.info("Não foi encontrado o paciente de id: " + id);
             throw new ResourceNotFoundException("Não foi encontrado o paciente buscado com id: " + id);
         }
     }
@@ -44,8 +52,12 @@ public class PacienteController {
     @PutMapping("/atualizar")
     public ResponseEntity<Paciente> alterarPaciente(@RequestBody Paciente paciente) throws BadRequestException {
         try {
+            service.getById(paciente.getId());
+            service.update(paciente);
+            log.info("Paciente de id: " + paciente.getId() + " foi atualizado com sucesso.");
             return ResponseEntity.ok(service.update(paciente));
         } catch (Exception e) {
+            log.info("Não foi possível atualizar o paciente.");
             throw new BadRequestException("Não foi possível atualizar os dados do paciente com os dados desta requisição");
         }
     }
@@ -55,8 +67,10 @@ public class PacienteController {
         try {
             ResponseEntity.ok(service.getById(id));
             service.remove(id);
+            log.info("O paciente de id: " + id + " foi localizado no banco de dados e removido com sucesso.");
             return ResponseEntity.ok("Paciente excluído.");
         } catch (Exception e) {
+            log.info("Não foi encontrado o paciente de id " + id + " para efetuar a exclusão.");
             throw new ResourceNotFoundException("Não foi possível excluir o paciente de id: " + id);
         }
     }
